7085479894cf128a5d40130133f33b1027669965
pawlugrz <grzegorz.pawluk@tieto.com>
2017-07-31 08:44:02 +0200

    func subtract

--- main.c
+++ main.c
@@ -10,11 +10,18 @@ int sum(int a, int b)
 	return a+b;
 }
 
+int subtract(int a, int b)
+{
+	return a-b;
+}
+
 int main()
 {
  int x=5, y=6;
  printf("Hello\n");
  printf("%d to the power = %d\n",x,power(5));
  printf("Sum %d and %d = %d\n",x,y,sum(x,y));
+ printf("Subtraction of %d and %d = %d\n",x,y,subtract(x,y));
  return 0;
 }
+
