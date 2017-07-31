f41566876cee5b713e100032d1906af8390d31d3
pawlugrz <grzegorz.pawluk@tieto.com>
2017-07-31 08:39:38 +0200

    func sum

--- main.c
+++ main.c
@@ -5,9 +5,16 @@ int power(int a)
 	return a*a;
 }
 
+int sum(int a, int b)
+{
+	return a+b;
+}
+
 int main()
 {
+ int x=5, y=6;
  printf("Hello\n");
- printf("%d\n",power(5));
+ printf("%d to the power = %d\n",x,power(5));
+ printf("Sum %d and %d = %d\n",x,y,sum(x,y));
  return 0;
 }
